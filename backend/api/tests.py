87152e69fed16a5b228d06b395742d45e5d516db
Peter Zernia <peter176@gmail.com>
2018-11-01 14:14:38 +0100

    started writing backend tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from api.models import Currency, Language, RegionalBloc, Country
 
-# Create your tests here.
+
+class CurrencyTest(TestCase):
+    def test_string_representation(self):
+        currency = Currency(
+            code='USD', name="United States dollar", symbol = "US$")
+        self.assertEqual(str(currency), currency.name)
