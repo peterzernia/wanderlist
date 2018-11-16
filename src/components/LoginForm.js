f84f0062e6cb85c5bf580c05e0dc807e71db9cbc
Peter Zernia <peter176@gmail.com>
2018-11-16 12:11:53 +0100

    added material cards to login and registration

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
 
 const LoginForm = (props) => (
-  <form onSubmit={props.handleSubmit}>
-    <TextField className="user-auth" type='text' name="username" label="Username"/><br/>
-    <TextField className="user-auth" type='password' name="password" label="Password"/><br/><br/>
-    <Button variant="contained" color="primary" type="submit">Login</Button>
-    <Link to="/register"><Button>Register</Button></Link>
-  </form>
+  <Card style={{ maxWidth: 400, margin: '0 auto'}}>
+    <CardHeader title="Login"/>
+    <CardContent>
+      <form onSubmit={props.handleSubmit}>
+        <TextField className="user-auth" type='text' name="username" label="Username"/><br/>
+        <TextField className="user-auth" type='password' name="password" label="Password"/><br/><br/>
+        <Button variant="contained" color="primary" type="submit">Login</Button>
+        <Link to="/register"><Button>Register</Button></Link>
+      </form>
+    </CardContent>
+  </Card>
 );
 
 export default LoginForm;
