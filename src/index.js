e50b5be4d69341516da1c3d8b9a2fa060efb9e30
Peter Zernia <peter176@gmail.com>
2018-11-19 13:57:06 +0100

    moved static css files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import './static/index.css';
+import './index.css';
 import store from "./store/index"
 import App from './App'
 import * as serviceWorker from './serviceWorker'
