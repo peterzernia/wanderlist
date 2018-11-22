f3137b9b393b0338d545fbbc82c7e0fbec1e52cb
Peter Zernia <peter176@gmail.com>
2018-11-22 18:33:54 +0100

    added slug field to trip report

--- trips/models.py
+++ trips/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from users.models import User
 from countries.models import Country
 from django.utils import timezone
+from django.utils.crypto import get_random_string
 
 
 class TripReport(models.Model):
@@ -10,6 +11,12 @@ class TripReport(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    slug = models.SlugField(max_length=12, unique=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = get_random_string(12,'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        super().save(*args, **kwargs)
