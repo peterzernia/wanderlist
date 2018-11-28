0c8e6e45e413be9d80211213b938b2dd5765eaa6
Peter Zernia <peter176@gmail.com>
2018-11-28 12:45:34 +0100

    added trip report image to thumbnail

--- backend/urls.py
+++ backend/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
