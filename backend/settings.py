32025bb15496ecf8ca5a80b016f3fcaeda1f2cbb
Peter Zernia <peter176@gmail.com>
2018-11-30 15:11:07 +0100

    debug false

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('COUNTRIES')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'w4nderlist.herokuapp.com']
 
