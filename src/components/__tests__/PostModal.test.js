5a02eb98f336e3d93a88c99d9711106112102a9f
Peter Zernia <peter176@gmail.com>
2018-12-04 16:04:39 +0100

    post modal test update

--- src/components/__tests__/PostModal.test.js
+++ src/components/__tests__/PostModal.test.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import PostModal from '../PostModal'
 import ReactModal from 'react-modal'
+import IconButton from '@material-ui/core/IconButton'
 
 describe('<PostModal />', () =>{
   it('closes modal', () => {
-    const wrapper = shallow(<PostModal showPostModal={true} />);
+    const closePostModal = jest.fn();
+    const wrapper = shallow(<PostModal closePostModal={closePostModal} showPostModal={true} />);
     expect(wrapper.find(ReactModal).length).toEqual(1);
+    wrapper.find(IconButton).simulate('click');
+    expect(closePostModal).toHaveBeenCalledTimes(1);
   });
 });
