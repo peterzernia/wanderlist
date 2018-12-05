4d8dc216c297c38804749030533ed859ad4eead1
Peter Zernia <peter176@gmail.com>
2018-12-05 17:24:12 +0100

    autocomplete tests in searchbar component

--- src/components/__tests__/SearchBar.test.js
+++ src/components/__tests__/SearchBar.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import SearchBar from '../SearchBar'
+import Autosuggest from 'react-autosuggest'
 
 describe('<SearchBar />', () =>{
   it('handleSubmit', () => {
@@ -8,5 +9,19 @@ describe('<SearchBar />', () =>{
     const wrapper = mount(<SearchBar handleSubmit={handleSubmit} />);
     wrapper.find('Button').simulate('submit');
     expect(handleSubmit).toHaveBeenCalledTimes(1);
+  });
+  it('gives correct autocomplete suggestions', () => {
+    const wrapper = mount(<SearchBar />);
+    expect(wrapper.state().suggestions).toEqual([]);
+    wrapper.setState({ value: 'd' });
+    wrapper.find('input').simulate('focus');
+    expect(wrapper.state().suggestions).toEqual([
+      {"name": "Denmark", "pk": 63},
+      {"name": "Djibouti", "pk": 64},
+      {"name": "Dominica", "pk": 65},
+      {"name": "Dominican Republic", "pk": 66}]);
+    wrapper.setState({ value: 'x' });
+    wrapper.find('input').simulate('focus');
+    expect(wrapper.state().suggestions).toEqual([]);
   });
 });
