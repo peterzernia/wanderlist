db24aa50aaebf2c080ada29e855beafd41bc402c
Peter Zernia <peter176@gmail.com>
2018-12-07 11:06:21 +0100

    country action creator tests

---
+++ src/actions/__test__/countryActions.test.js
@@ -0,0 +1,20 @@
+import * as countryActions from '../countryActions'
+import country from '../../testVariables'
+
+describe('Country Actions', () => {
+  it('should create a FETCH_COUNTRY_PENDING action', () => {
+    const expectedAction = {type: 'FETCH_COUNTRY_PENDING'}
+    expect(countryActions.fetchCountryPending()).toEqual(expectedAction)
+  });
+  it('should create a FETCH_COUNTRY_FULFILLED action', () => {
+    const expectedAction = {
+      type: 'FETCH_COUNTRY_FULFILLED',
+      country
+    }
+    expect(countryActions.fetchCountryFulfilled(country)).toEqual(expectedAction)
+  });
+  it('should create a FETCH_COUNTRY_REJECTED action', () => {
+    const expectedAction = {type: 'FETCH_COUNTRY_REJECTED'}
+    expect(countryActions.fetchCountryRejected()).toEqual(expectedAction)
+  });
+});
