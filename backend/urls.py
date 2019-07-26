3919d762a8e1f6b2cdcf7a482f61df3e3df4d742
peterzernia <peter.zernia@medigo.com>
2019-07-26 11:48:34 +0200

    removed media url

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path,re_path, include
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,5 +10,3 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
