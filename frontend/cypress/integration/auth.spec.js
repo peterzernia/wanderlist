7882d9fcd41f976103779dab2e2477f0ce773a06
peterzernia <peter.zernia@medigo.com>
2019-08-02 11:44:57 +0200

    integration tests

---
+++ frontend/cypress/integration/auth.spec.js
@@ -0,0 +1,35 @@
+describe('Authentication', () => {
+  it('registers a user', () => {
+    cy.visit('/register')
+    cy.get('[name="username"]')
+      .type('TestUser')
+    cy.get('[name="email"]')
+      .type('test@test.com')
+    cy.get('[name="password1"]')
+      .type('testing1234')
+    cy.get('[name="password2"]')
+      .type('testing1234')
+    cy.get('.jss129')
+      .click({ multiple: true })
+    cy.contains('Aland Islands')
+      .click()
+    cy.get('[type="submit"]')
+      .click()
+
+    cy.contains('You have successfully registered.')
+  })
+
+  it('logs out', () => {
+    cy.visit('/logout')
+  })
+
+  it('logs in', () => {
+    cy.visit('/login')
+    cy.get('[name="username"]')
+      .type('TestUser')
+    cy.get('[name="password"]')
+      .type('testing1234')
+    cy.get('[type="submit"]')
+      .click()
+  })
+})
---
+++ frontend/cypress/integration/navbar.spec.js
@@ -0,0 +1,17 @@
+describe('Navigating with the navbar', () => {
+  it('Navigates to feed page', () => {
+    cy.visit('/')
+    cy.get('[title="Trip Report Feed"]').click()
+    cy.url().should('include', '/feed')
+  })
+
+  it('Navigates to search page', () => {
+    cy.get('[title="Search for Countries"]').click()
+    cy.url().should('include', '/search')
+  })
+
+  it('Navigates to profile page', () => {
+    cy.get('[title="My Profile"]').click()
+    cy.url().should('include', '/login') // Originally not authenticated
+  })
+})
