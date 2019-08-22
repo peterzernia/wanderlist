e49b12c1738bc705cf33a6b0c81ad7b6895613c1
peterzernia <peter.zernia@medigo.com>
2019-08-22 10:25:13 +0200

    added travis db settings back for testing

--- backend/settings.py
+++ backend/settings.py
@@ -160,17 +160,17 @@ DATABASES['default'] = dj_database_url.config()
 
 
 # Travis ci database settings.
-# if 'TRAVIS' in os.environ:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'travis_ci_test',
-#             'USER': 'postgres',
-#             'PASSWORD': '',
-#             'HOST': 'localhost',
-#             'PORT': '',
-#         }
-#     }
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'travis_ci_test',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Local settings.
