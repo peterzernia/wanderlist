4313e78a43ece9bdca4ea8a79fab5b155eff2222
peterzernia <peter.zernia@medigo.com>
2019-08-22 17:08:03 +0200

    refactored RegistrationForm to a functional component

--- frontend/src/components/PostModal.js
+++ frontend/src/components/PostModal.js
@@ -8,7 +8,7 @@ import Close from '@material-ui/icons/Close'
 ReactModal.setAppElement('body');
 
 /*
-this.props.updatePostModal is updated depending on which action opens up the
+updatePostModal is updated depending on which action opens up the
 Post Modal. If the action is "OPEN_UPDATE_POST_MODAL" it flips to true, and a
 trip report is passed in that can be updated by the user. If the action is
 "OPEN_POST_MODAL", it remains false and no trip report is passed in, and a new
--- frontend/src/components/RegistrationForm.js
+++ frontend/src/components/RegistrationForm.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
+import { bool, func } from 'prop-types'
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
@@ -12,55 +13,51 @@ import FormControl from '@material-ui/core/FormControl'
 import countries from '../country_data'
 import { DotLoader } from 'react-spinners'
 
-class RegistrationForm extends Component {
+export default function RegistrationForm(props) {
+  const [country, setCountry] = useState('')
+  const { authenticating, handleSubmit} = props
 
-  constructor() {
-    super();
-    this.state = {
-      country: ''
-    }
-  }
-
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+  const handleChange = event => {
+    setCountry(event.target.value)
   };
 
-  render() {
 
-    /*
-    Importing JSON file with all of the country names and pks from the Django
-    database. This array is mapped over to create the options of the Matrial
-    UI select form.
-    */
-    const menuItems = [...countries].sort((a, b) => a.name > b.name).map(country => (
-      <MenuItem key={country.pk} value={country.pk}>{country.name}</MenuItem>
-    ))
+  /*
+  Importing JSON file with all of the country names and pks from the Django
+  database. This array is mapped over to create the options of the Matrial
+  UI select form.
+  */
+  const menuItems = [...countries].sort((a, b) => a.name > b.name).map(country => (
+    <MenuItem key={country.pk} value={country.pk}>{country.name}</MenuItem>
+  ))
 
-    return(
-      <div>
-        {this.props.authenticating && <div><DotLoader size={50} color={'#2196f3'} className="content" /><br/></div>}
-        <Card style={{ maxWidth: 400, margin: '0 auto'}}>
-          <CardHeader title="Register"/>
-          <CardContent>
-            <form onSubmit={this.props.handleSubmit}>
-              <TextField className="user-auth" type='text' name="username" label="Username" required/><br/>
-              <TextField className="user-auth" type='text' name="email" label="Email" required/><br/>
-              <TextField className="user-auth" type='password' name="password1" label="Password" required/><br/>
-              <TextField className="user-auth" type='password' name="password2" label="Confirm Password" required/><br/>
-              <FormControl>
-                <InputLabel htmlFor="country">Home Country</InputLabel>
-                <Select style={{ textAlign: 'left'}} className="user-auth" name="country" onChange={this.handleChange} value={this.state.country}>
-                  {menuItems}
-                </Select>
-              </FormControl><br/><br/>
-              <Button variant="contained" color="primary" type="submit">Register</Button>
-              <Link to="/login"><Button>Cancel</Button></Link>
-            </form>
-          </CardContent>
-        </Card>
-      </div>
-    )
-  }
-};
+  return(
+    <div>
+      {authenticating && <div><DotLoader size={50} color={'#2196f3'} className="content" /><br/></div>}
+      <Card style={{ maxWidth: 400, margin: '0 auto'}}>
+        <CardHeader title="Register"/>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <TextField className="user-auth" type='text' name="username" label="Username" required/><br/>
+            <TextField className="user-auth" type='text' name="email" label="Email" required/><br/>
+            <TextField className="user-auth" type='password' name="password1" label="Password" required/><br/>
+            <TextField className="user-auth" type='password' name="password2" label="Confirm Password" required/><br/>
+            <FormControl>
+              <InputLabel htmlFor="country">Home Country</InputLabel>
+              <Select style={{ textAlign: 'left'}} className="user-auth" name="country" onChange={handleChange} value={country}>
+                {menuItems}
+              </Select>
+            </FormControl><br/><br/>
+            <Button variant="contained" color="primary" type="submit">Register</Button>
+            <Link to="/login"><Button>Cancel</Button></Link>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
 
-export default RegistrationForm;
+RegistrationForm.propTypes = {
+  authenticating: bool,
+  handleSubmit: func,
+}
