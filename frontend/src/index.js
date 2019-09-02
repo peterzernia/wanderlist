fc593461003caca23e58e0e6adf03ff74e916ca9
peterzernia <peter.zernia@medigo.com>
2019-09-02 16:58:11 +0200

    added sentry

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import './index.css'
+import * as Sentry from '@sentry/browser'
 import store from './store/index'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_HOST,
+  environment: process.env.NODE_ENV,
+})
 
 ReactDOM.render(
   <Provider store={store}>
